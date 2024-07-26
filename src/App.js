ded80445a06806076745fac965f999633d3db79e
lux <52521726+LuxLMAO@users.noreply.github.com>
2024-07-25 23:33:58 -0500

    .

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="">
-        
+        <div className="top-text">
+          <p>I hate the way you fucking look at me.</p>
+        </div>
       </header>
       <main>
+        <div className="main-card">
+
+        </div>
       </main>
       <footer>
-        
+        <p>Quote</p>
       </footer>
     </div>
   );
